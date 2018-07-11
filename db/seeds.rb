4f72553ad21419c022117a24d780ca1d179dde4a
thesecretmaster <thesecretmaster@developingtechnician.com>
2018-07-11 18:20:24 -0400

    New emojis (v3)

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 ReviewResult.create([
-  {name: "I feel fine or neutral", emoji: "https://i.stack.imgur.com/OZ2cB.png"}, # "\u{1F642}"
-  {name: "I feel unsure or annoyed", emoji: "https://i.stack.imgur.com/zECqZ.png"}, # "\u{1F928}"
-  {name: "I feel angry or upset", emoji: "https://i.stack.imgur.com/T6Er1.png"} # "\u{1F92C}"
+  {name: "I feel fine or neutral", emoji: "https://i.stack.imgur.com/f22Ua.png"}, # "\u{1F642}"
+  {name: "I feel unsure or annoyed", emoji: "https://i.stack.imgur.com/gsUPY.png"}, # "\u{1F928}"
+  {name: "I feel angry or upset", emoji: "https://i.stack.imgur.com/1YKEn.png"} # "\u{1F92C}"
 ])
